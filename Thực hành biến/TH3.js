78cd8c6e57481500bbef9cec0d3b2bbefeb0ea57
unknown <hoangchien.05052002@gmail.com>
2022-05-09 14:03:58 +0700

    THB

---
+++ Thực hành biến/TH3.js
@@ -0,0 +1,6 @@
+inputwidth=prompt("enter the width");
+inputheught=prompt("enter the weight");
+let width= parseInt(inputwidth);
+let height= parseInt(inputheught);
+let area = width * height;
+document.write('the area is: '+ area )
